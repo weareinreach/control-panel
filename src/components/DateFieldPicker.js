1195ead0c9bc9f1464d5d6e1716c6590ef7a5f2e
Joshua Lokken <jrrlokken@gmail.com>
2021-05-12 12:47:27 -0500

    Removed unused imports from DateFieldPicker

--- src/components/DateFieldPicker.js
+++ src/components/DateFieldPicker.js
@@ -1,22 +1,11 @@
 import React from 'react';
 import DatePicker from 'react-datepicker';
-import {
-  Checkbox,
-  FormControl,
-  FormErrorMessage,
-  FormLabel,
-  Icon,
-  Input,
-  Select,
-  Text,
-  Textarea,
-  Tooltip,
-} from '@chakra-ui/core';
+import { FormControl } from '@chakra-ui/core';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../date-picker.css';
 
 
-const DateFieldPicker = ({ onChange, selected, onSelect }) => {
+const DateFieldPicker = ({ onChange, selected }) => {
   return (
     <FormControl>
         <DatePicker 

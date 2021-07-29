6970e7c0624988464d9182c655cb9f06680ca8e0
Joshua Lokken <joshualokken@pm.me>
2021-07-29 12:58:56 -0500

    Passed additional props to react-datepicker

--- src/components/DateFieldPicker.js
+++ src/components/DateFieldPicker.js
@@ -4,10 +4,22 @@ import {FormControl} from '@chakra-ui/react';
 import 'react-datepicker/dist/react-datepicker.css';
 import '../date-picker.css';
 
-const DateFieldPicker = ({onChange, selected, placeholderText}) => {
+const DateFieldPicker = ({
+  onChange,
+  selected,
+  placeholderText,
+  minDate,
+  maxDate,
+  required,
+  disabled,
+}) => {
   return (
     <FormControl>
       <DatePicker
+        disabled={disabled}
+        required={required}
+        minDate={minDate}
+        maxDate={maxDate}
         placeholderText={placeholderText}
         showPopperArrow={true}
         selected={selected}

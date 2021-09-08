b34fbe5bce2d6e20be3428f08c948963ec2c1ef1
Alfred <devops.moreira@gmail.com>
2021-09-07 18:10:39 -0600

    Update login_page.js

--- cypress/support/reusable_tests/login_page.js
+++ cypress/support/reusable_tests/login_page.js
@@ -94,7 +94,7 @@ Cypress.Commands.add('testLogOut',(viewport,username,password)=>{
             cy.wrap($element[1]).click();
         });
     });
-    cy.wait(1000);
+    cy.wait(2000);
     //Verify Logged Out
     cy.getElementByTestId('layout').then($element =>{
         expect($element).to.be.visible;

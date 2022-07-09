f3f73860fb5962479c06c41bce18257d67e2d5c3
trigal <10123216+trigal2012@users.noreply.github.com>
2022-07-08 19:29:14 -0700

    adding helper text to top of Tags page

--- src/pages/Service.js
+++ src/pages/Service.js
@@ -539,6 +539,10 @@ const Service = (props) => {
           <TabPanel marginTop={2}>
             <Stack spacing={4}>
               <Container>
+                <p>What <b>“tag(s)”</b> you choose affect(s) when this organization appears in the search results of our free App. For example, if you add a “LGBTQ Centers” tag to a service page, the organization will appear for a user who searches for “Community Support - LGBTQ Centers” in the App. Please try to consider the user’s perspective when entering tag(s).</p>
+                <p><b>*Note:</b> Please also make sure to separate an organization's services into unique service pages. This is especially true for legal services. For example, if an organization offers both gender/name change legal services and asylum legal services, these should actually be separate service pages in the data portal (each service page should then have its own distinct, relevant tag). Similarly, if you are using two very different tag types (e.g. transportation and legal), you should likely instead create two different service pages (each with their own distinct tag). Please post in the #community-outreach channel in Slack with any questions.</p>
+              </Container>
+              <Container>
                 <Box {...buttonGroupProps}>
                   <Button onClick={openEditTags('united_states')} data-test-id="service-us-tags-button">
                     Edit Tags
